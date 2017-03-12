a48b49b3935b4057a378d0eb49cdc8eb5795b178
Nitescu Lucian <nitesculuci@gmail.com>
2017-03-12 23:50:12 +0200

    Create kali.sh

---
+++ kali.sh
@@ -0,0 +1,23 @@
+apt-get clean
+apt-get update 
+apt-get upgrade -y 
+apt-get dist-upgrade -y
+passwd
+apt-get install chkrootkit
+leafpad /etc/hostname
+leafpad /etc/hosts
+updatedb
+cat /etc/shadow | awk -F: '($2==""){print $1}' > ./no_password_users.txt
+echo Running chkrootkit. Wait!
+sudo chkrootkit > ./chkrootkit_log.txt
+apt-get install lynis
+apt-get -f install
+apt-get install lynis
+echo Running lynis. Wait! 
+lynis audit system > ./lynis_log.txt
+netstat -tulpn > ./open_ports_log.txt
+echo Close Unwanted Ports using: iptables -A INPUT -p tcp --dport PORT_NUMBER -j DROP 
+iptables -L -n -v > ./iptables_log.txt
+apt-get install rkhunter
+rkhunter --update
+rkhunter -c
