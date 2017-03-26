035cddcb5525c7bc0b47327b29999780fd323799
NitescuLucian <nitesculuci@gmail.com>
2017-03-26 15:02:45 +0300

    Option to prevent IP spoofing

--- ubuntu_16.04LTS.sh
+++ ubuntu_16.04LTS.sh
@@ -56,6 +56,13 @@ echo net.ipv4.icmp_echo_ignore_all = 1 >> /etc/sysctl.conf
 echo "${yellow}Ignore Broadcast Request ${reset}"
 echo net.ipv4.icmp_echo_ignore_broadcasts = 1 >> /etc/sysctl.conf
 sysctl -p
+#Preventing IP spoofing
+echo "${yellow}Would you like to prevent IP spoofing? (This will reset you /etc/host.conf) (y/n)${reset}"
+read foe
+if [ "$foe" = "y" ]; then
+echo order bind,hosts > /etc/host.conf
+echo nospoof on >> /etc/host.conf
+fi
 
 #Specific System Security
 echo "${yellow}Would you like to secure shared memory? Keep in mind that a reboot is required. (y/n)${reset}"
